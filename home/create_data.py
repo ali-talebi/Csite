b2fae841d69be96bb9ac59ab74620b6d769d045d
ali-talebi <alitalebishahroodi@gmail.com>
2024-06-29 17:05:12 +0000

    add create_data.py and list.txt

---
+++ home/create_data.py
@@ -0,0 +1,11 @@
+import os
+def Create_data(path) :
+
+    with open( path , "r" ) as file :
+        for line in file.readlines() :
+
+            new_line = f"https://{line}"
+            yield new_line
+
+
+
